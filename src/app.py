83d00801c106fabc6d40232ff0d16ace9b85a50d
Bonte Emma BANA <bbonteemma@gmail.com>
2021-01-29 19:03:15 +0200

    changes

--- src/app.py
+++ src/app.py
@@ -1,23 +1,10 @@
 import sys
 import os
-
-
-def palindrome(string):
-	left_pos = 0
-	right_pos = len(string) - 1
-	while right_pos >= left_pos:
-		if not string[left_pos] == string[right_pos]:
-			return False
-		left_pos += 1
-		right_pos -= 1
-	return True
-print(palindrome('radar')) 
+def palindrome(s):
+	return s[::-1] == s
 def solution(s):
     return palindrome(s)
-
-
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         sys.exit(os.error("Argment required"))
-
     print(solution(sys.argv[1]))
